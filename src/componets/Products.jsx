523e5ec3a9c586cfdea03985a13fae847cb0f07e
Mma <chidimma.okafor.c@gmail.com>
2022-05-20 17:24:11 +0100

    Using query string with react-router

--- src/componets/Products.jsx
+++ src/componets/Products.jsx
@@ -8,6 +8,22 @@ const Products = () => {
         <input type="text" placeholder="Search Products" />
       </div>
       <nav style={{ padding: "16px 32px" }}>
+        {/* a relative link is a Link whose "to" prop value is without / e.g "featured" and not "/featured"
+            why have a relative link ==> used when you have nested routes in the parent component, so you use it when defining Link tags to those nested routes
+            what happens when you do this? react router appends the link "to" value to the route "path" value. the route part of the component in which the Link was defined. so in our case, this
+
+            <Link to="featured">
+                Featured
+            </Link>
+
+            gets translated to /products/featured
+            because this Link is defined in the Products component's page and the route for the component has a /products path,
+
+            In essence, relative links inherit the closest route in which they are rendered. very useful when you have to link deeply nested paths
+
+            An absolute link on the other hand is defined with / slash and react router will construct the path from the root of the app and not the current URL, so /new ==> http://localhost:3000/new.
+            absolute link is used mainly for primary naviagtion link
+          */}
         <Link to="featured" style={{ marginRight: "16px" }}>
           Featured
         </Link>
@@ -15,7 +31,8 @@ const Products = () => {
           New
         </Link>
       </nav>
-      {/* outlet component allows the parent component to display the child component that matches the url, e.g /products/new ==> will display the <NewProducts/> component */}
+      {/* outlet component allows the parent component to display the child component that matches the url, e.g /products/new ==> will display the <NewProducts/> component 
+      */}
       <Outlet />
     </>
   );
--- src/componets/Users.jsx
+++ src/componets/Users.jsx
@@ -1,13 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Outlet, useSearchParams } from "react-router-dom";
 
 const Users = () => {
+  // to use and update search params(same as query strings), react provides a hook called useSearchParams(). this hook is just like useState except that the value is stored iin the URL while the value for useState is stored in memory
+  //this hook returns an array whose first element is an object(that contains all the query strings in a key:value pair) and a setter function to update the search params
+  //this hook gives you access to fetch or update the search params, then depending on the value of object in time, you can conditionally render your components
+
+  /*
+        also note that you can type in query strings in the url and the searchParams object will be updated to those new values
+        you can also add a button to the components that when clicked will call the setSearchParams method to update the searchParams object(which will update the url)
+
+        STEPS
+        import the useSearchParmac() hook
+        call it and use the returned values accordingly
+        update the object by getting one or more of the components to perform an action
+        get the current value of the object and conditionally render the components based on that value
+    */
+  const [searchParams, setSearchParams] = useSearchParams();
+  const showActiveUsers = searchParams.get("filter") === "active"; //the query string key is filter and its value is active ==> ?filter=active
   return (
-    <>
+    <div>
+      <div>
         <h1>User 1</h1>
         <h1>User 2</h1>
         <h1>User 3</h1>
-    </>
-  )
-}
+      </div>
+      {/* for the nested routes. so that the parent component can display the child components when the url is matched*/}
+      <Outlet />
+      <div>
+        <button onClick={() => setSearchParams({ filter: "active" })}>
+          Active Users
+        </button>
+        <button onClick={() => setSearchParams({})}>Reset Filter</button>
+      </div>
+      {/* based on the value of the filter, conditionally render the components */}
+      {showActiveUsers ? (
+        <h1>Showing Active Users</h1>
+      ) : (
+        <h1>Showing All Users</h1>
+      )}
+    </div>
+  );
+};
 
-export default Users
+export default Users;
